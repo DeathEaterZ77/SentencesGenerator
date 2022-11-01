8f6fa17ab6d63cdd21a7d9c040c43ba40ee59f3e
charlineblanc <charline.blanc@efrei.net>
2022-11-01 19:05:49 +0100

    ouvrir un fichier

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+FILE* fopen(const char* dico_10_lignes, const char* r);
+int main(int argc, char *argv[])
+{
+    FILE* fichier = NULL;
+    fichier = fopen("dico_10_lignes.txt", "r");
+
+    return 0;
+}

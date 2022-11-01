8c984d8a299f417e018cbd61952896066e89cc84
charlineblanc <charline.blanc@efrei.net>
2022-11-01 19:24:23 +0100

    ouvrir un fichier

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX_SIZE 1000
 
-FILE* fopen(const char* dico_10_lignes, const char* r);
-int main(int argc, char *argv[])
-{
-    FILE* fichier = NULL;
-    fichier = fopen("dico_10_lignes.txt", "r");
+int main() {
+    FILE* dico_test = NULL;// create a pointer of type file & initialize it to NULL
+    char str[MAX_SIZE]="";
 
+    dico_test = fopen("../dico.txt","r");//open the file
+
+    if (dico_test == NULL) // test if the file is NULL
+        printf("Cannot open this file");
+
+    while(fgets(str,MAX_SIZE,dico_test)!=NULL)
+    {
+        printf("%s",str);
+    }
     return 0;
 }

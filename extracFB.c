9d570b9b5fa1de50d369c6eb7f4ed2b473c53aa1
charlineblanc <charline.blanc@efrei.net>
2022-11-03 18:50:11 +0100

    première version du menu
    - lire les lignes du dico : ok
    - extraire les formes flechies : ok
    - extraire les formes de base : ok
    - extraire les categories : ok
    - extraire les infos : NOPE

---
+++ extracFB.c
@@ -0,0 +1,37 @@
+//
+// Created by charl on 03/11/2022.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+char extracFB() {
+    FILE* dico_test;
+    char str [1000];
+    short nb_lignes_lues, nb_val_lues;
+    char forme_flechie[400];
+    char forme_base[400];
+    char nature[200];
+    char genre_nbr_tps[200];
+
+    dico_test = fopen("../dico.txt","r");//open the file
+
+    if (dico_test == NULL) {// test if the file is NULL
+        printf("Cannot open this file");
+        exit(0);
+    }
+
+    while(fgets(str,1000,dico_test)!=0 && str[0]=="$"); // read the file while it's not NULL
+    nb_lignes_lues = 0;
+    while (fgets(str,1000, dico_test) != NULL) {
+        nb_lignes_lues++;
+        nb_val_lues = sscanf(str, "%s%s%s", forme_flechie, forme_base, nature);
+        if (nb_val_lues !=3)
+            printf(" PROBLEME \n", nb_lignes_lues);
+        else
+            printf("(Ligne %2hd) %s \n", nb_lignes_lues, forme_base);
+    }
+    fclose(dico_test);
+}
+
+
--- main.c
+++ main.c
@@ -1,19 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX_SIZE 1000
 
-int main() {
-    FILE* dico_test = NULL;// create a pointer of type file & initialize it to NULL
-    char str[MAX_SIZE]="";
+int affichageMenu(void) /* pas d'entree donc void */
+{
+    int choixMenu; /* il manquait un point virgule */
 
-    dico_test = fopen("../dico.txt","r");//open the file
-
-    if (dico_test == NULL) // test if the file is NULL
-        printf("Cannot open this file");
-
-    while(fgets(str,MAX_SIZE,dico_test)!=NULL)
+    printf("---Menu---\n\n");
+    printf("1.Afficher les lignes du dico!\n");
+    printf("2.extraction des formes de base\n");
+    printf("3.extraction formes flechies\n");
+    printf("4.extraction nature du mot\n");
+    printf("5.extraction categorie du mot\n");
+    printf("\nVotre choix?\n\n");
+    scanf("%d", &choixMenu);
+    return choixMenu; /* c'est ici qu'on retourne une valeur*/
+}
+int main(void)
+{
+    switch (affichageMenu())
     {
-        printf("%s",str);
+        case 1:
+            readDico();
+            break;
+        case 2:
+            extracFF();
+            break;
+        case 3:
+            extracFB();
+            break;
+        case 4:
+            extracInfos();
+            break;
+        case 5:
+            extracCat();
+            break;
+        default:
+            printf("Vous ne ferez rien du tout!");
+            break;
     }
     return 0;
 }
+
+
+

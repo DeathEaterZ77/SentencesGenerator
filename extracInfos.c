5b1dee426fe53a6356e4e6094d543f4fccb9e67a
Doriane <100567151+Dorianefk@users.noreply.github.com>
2022-11-03 21:16:10 +0100

    Update extracInfos.c

--- extracInfos.c
+++ extracInfos.c
@@ -6,32 +6,54 @@
 #include <stdio.h>
 
 char extracInfos() {
+
+    //defining the file
     FILE* dico_test;
+    
+    // this array is needed to memorize the line read by the file
     char str [1000];
+    
+    // counters for the second test made below
     short nb_lignes_lues, nb_val_lues;
+    
+    // arrays containing the different categories of words
     char forme_flechie[400];
     char forme_base[400];
     char nature[200];
     char genre_nbr_tps[200];
 
-    dico_test = fopen("../dico.txt","r");//open the file
+    // open the file
+    dico_test = fopen("../dico.txt","r");
 
-    if (dico_test == NULL) {// test if the file is NULL
+    // test if the file is NULL
+    if (dico_test == NULL) {
         printf("Cannot open this file");
         exit(0);
     }
 
-    while(fgets(str,1000,dico_test)!=0 && str[0]=="$"); // read the file while it's not NULL
+    // avoid all the lines that starts with $
+    while(fgets(str,1000,dico_test)!=0 && str[0]=="$"); 
+    
     nb_lignes_lues = 0;
+    
+    // read the file while it's not NULL
     while (fgets(str,1000, dico_test) != NULL) {
+    
         nb_lignes_lues++;
+        
+        // analyse the line and keep the 3 categories of words
+        // that we want to extract from the file
         nb_val_lues = sscanf(str, "%s%s%s", forme_flechie, forme_base, nature);
+        
+        // we check if we sscanf function took the 3 categories needed
         if (nb_val_lues !=3)
             printf(" PROBLEME \n", nb_lignes_lues);
         else
-            printf("(Ligne %2hd) %30.4s \n", nb_lignes_lues, nature);
+            
+            printf("(Ligne %2hd) %0.100s \n", nb_lignes_lues, nature);
 
     }
+    // close the file 
     fclose(dico_test);
 }
 

903865a4aaa5c793f4d05351b6b9db59f67e4d99
Carla Gros-Alcaraz <carlagros@free.fr>
2022-11-04 02:37:18 +0100

    base of data structure with node creation

--- node.c
+++ node.c
@@ -3,3 +3,21 @@
 //
 
 #include "node.h"
+
+pflech_node createNodeFlech(char flech_word, char genre_nbr_tps){
+    pflech_node new;
+    new->flech_word = flech_word;
+    new->attributs = genre_nbr_tps;
+    new->next = NULL;
+    return new;
+}
+
+pbase_node createNode(char val){
+    pbase_node new;
+    new->letter = val;
+    new->next = new->son = NULL;
+    new->end = 0;
+    new->depth = -1;
+    new->flechies = NULL;
+    return new;
+}
--- node.h
+++ node.h
@@ -5,4 +5,35 @@
 #ifndef SENTENCESGENERATOR_NODE_H
 #define SENTENCESGENERATOR_NODE_H
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+
+// data structures of 2 types of nodes
+
+struct flech_node{ // inflected form node collection
+    char flech_word; // inflected form
+    char attributs; // gender, number, time
+    struct flech_node *next;
+};
+typedef struct flech_node tflech_node, *pflech_node;
+
+struct base_node{ // based forms node collection
+    char letter;
+    struct base_node *next, *son;
+    int end; // if we reach the end of the based word
+    int depth;
+    pflech_node flechies; // associated inflected forms
+};
+typedef struct base_node tbase_node, *pbase_node;
+
+
+// functions implemented
+pflech_node createFlechNode(char,char);
+pbase_node createNode(char);
+
+
+
+
 #endif //SENTENCESGENERATOR_NODE_H

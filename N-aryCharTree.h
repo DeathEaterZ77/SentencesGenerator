e43897581b5ab46ab2e1cfce8979f52cfe09344d
Carla Gros-Alcaraz <carlagros@free.fr>
2022-11-05 01:46:50 +0100

    add displaytree - functions by NF - and narychartree

---
+++ N-aryCharTree.h
@@ -0,0 +1,20 @@
+//
+// Created by Carla Gros-Alcaraz on 05/11/2022.
+//
+
+#ifndef SENTENCESGENERATOR_N_ARYCHARTREE_H
+#define SENTENCESGENERATOR_N_ARYCHARTREE_H
+
+#include <stddef.h>
+#include "node.h"
+
+struct n_tree{
+    pbase_node root;
+};
+typedef struct n_tree nt_tree, *np_tree;
+
+//implemented functions
+
+
+
+#endif //SENTENCESGENERATOR_N_ARYCHARTREE_H

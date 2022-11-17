4dfa575d9b3ea4e797387238b61fefbf82559be7
charline blanc <103666967+charlineblanc@users.noreply.github.com>
2022-11-17 09:14:54 +0100

    Update N-aryCharTree.c

--- N-aryCharTree.c
+++ N-aryCharTree.c
@@ -18,13 +18,15 @@ void fill_NTrees( nt_tree* cat_trees ){
 
     char basic_form[100], flech_form[100], infos[100];
 
-    // faire une boucle pour lire chaque ligne du fichier ouvert
+    // do a while loop to read each line of the open file
     while(fscanf(dico," %s %s %s ", flech_form,basic_form, infos) == 3 ) {
-        //verifier dans quel arbre doit aller la ligne
+        // verify in which tree each line must go
         char catvalue[4];
         for(int i=0; i<3; i++) catvalue[i]=infos[i];
+        // '\0' is for the end of the string
         catvalue[3] = '\0';
-        if(strcmp(catvalue,"Nom") == 0) addWordToTree(basic_form, flech_form, infos + 4, &cat_trees[0]);
+        // we compare catvalue with the category and then add to the right tree
+        if(strcmp(catvalue,"Nom") == 0) addWordToTree(basic_form, flech_form, infos + 4, &cat_trees[0]); 
         else if(strcmp(catvalue,"Ver") == 0) addWordToTree(basic_form, flech_form, infos + 4, &cat_trees[1]);
         else if(strcmp(catvalue,"Adj") == 0) addWordToTree(basic_form, flech_form, infos + 4, &cat_trees[2]);
         else if(strcmp(catvalue,"Adv") == 0) addWordToTree(basic_form, flech_form, infos + 4, &cat_trees[3]);
@@ -34,18 +36,22 @@ void fill_NTrees( nt_tree* cat_trees ){
     fclose(dico);
 }
 
+
 void addWordToTree(char* basic_form, char* flech_form, char* gender_nbr_time, nt_tree* cat){
     pbase_node here = cat->root;
     int i=0, search;
     pbase_node temp;
+    // using a while loop to go further in the tree 
     while(basic_form[i] != '\0'){
         temp = here->son;
         if(temp==NULL){
+            // if son is NULL we add the new node in the son
             addNode(here, basic_form[i], 1);
             here->nbsons++;
             here = here->son;
         }
         else {
+            // if not we add the new node to the sibling
             search = 0;
             while (search == 0) {
                 if (temp->letter == basic_form[i]) search = 1;
